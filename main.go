b0d06f230b6e733bebb430b64964ed499e59dc4f
Nikita Merzlyakov <merzlyakov02@gmail.com>
2022-01-18 21:48:53 +0300

    init fiber

--- main.go
+++ main.go
@@ -1,4 +1,17 @@
 package main
 
+import (
+	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/logger"
+)
+
 func main() {
+	app := fiber.New()
+	app.Use(logger.New())
+
+	app.Get("/", func(ctx *fiber.Ctx) error {
+		return ctx.SendString("hello")
+	})
+
+	app.Listen(":8080")
 }
